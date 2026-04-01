9101d435833e66935f533d977b36415afe5111bd
Dagnis Skurbe <dagnis.skurbe@gmail.com>
2026-04-01 12:55:17 +0300

    refactor/starting ui library migration manually

--- src/components-ui/ColorThumb.tsx
+++
@@ -1,8 +0,0 @@
-import { ColorThumb as AriaColorThumb } from 'react-aria-components';
-import type { ColorThumbProps } from 'react-aria-components';
-
-import styles from './ColorThumb.module.sass';
-
-export function ColorThumb(props: ColorThumbProps) {
-  return <AriaColorThumb {...props} />;
-}
--- src/components-ui/Link.tsx
+++
@@ -1,7 +0,0 @@
-import { Link as RACLink } from 'react-aria-components';
-import type { LinkProps } from 'react-aria-components';
-import styles from './Link.module.sass';
-
-export function Link(props: LinkProps) {
-  return <RACLink {...props} />;
-}
