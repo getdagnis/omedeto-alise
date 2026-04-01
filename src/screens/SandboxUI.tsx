1d990e199a7a9d3c7513985aac4f037902902040
Dagnis Skurbe <dagnis.skurbe@gmail.com>
2026-04-01 19:09:24 +0300

    fix/large style paths update

--- src/screens/SandboxUI.tsx
+++ src/screens/SandboxUI.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumb, Breadcrumbs, Button } from '../components-ui';
+import { Breadcrumb, Breadcrumbs, Button, Separator } from '../components-ui';
 import styles from './SandboxUI.module.sass';
 
 export default function StyleGuide() {
@@ -9,10 +9,12 @@ export default function StyleGuide() {
       </div>
       <div className={styles.grid}>
         <div className={styles.card}>
+          <h3>UI bricks</h3>
           <h4>Button</h4>
-          <Button className={styles.button}>Button</Button>
-        </div>
-        <div className={styles.card}>
+          <div className={styles.preview}>
+            <Button className={styles.button}>Button</Button>
+          </div>
+          <Separator />
           <h4>Breadcrumbs</h4>
           <Breadcrumbs>
             <Breadcrumb href="#">Home</Breadcrumb>
@@ -20,6 +22,7 @@ export default function StyleGuide() {
             <Breadcrumb>Breadcrumbs</Breadcrumb>
           </Breadcrumbs>
         </div>
+        <div className={styles.card}></div>
       </div>
     </div>
   );
