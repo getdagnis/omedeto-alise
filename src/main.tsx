6649c6e42ebe5c74a9b2913c6e5ac44b5448a0f9
Dagnis Skurbe <dagnis.skurbe@gmail.com>
2026-04-03 03:34:40 +0300

    update/migration to new ui - ui library element styles restored

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/globals.sass';
+import './styles/theme.sass';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
